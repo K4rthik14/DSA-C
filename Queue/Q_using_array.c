2d39e998ed4c0a1869294cc1f8c5175314b54fb4
“[Karthik <karthiksaya14@gmail.com>
2025-08-02 00:20:07 +0530

    Updated enqueue logic

--- Queue/Q_using_array.c
+++ Queue/Q_using_array.c
@@ -13,3 +13,21 @@ Handle overflow and underflow conditions appropriately*/
 int queue[MAX]; // Array to store queue elements
 int front = -1; // Index of the front element
 int rear = -1;  // Index of the rear element
+// Function to check if the queue is empty
+int isEmpty(){
+    return (front == -1 && rear == -1);
+}
+
+void enqueue(int value){
+if (rear ==MAX -1){
+    printf("Queue Overfolw\n");
+    return;
+
+}
+if (front = -1)
+front = 0; // If queue is empty, set front to 0
+
+rear++;
+queue[rear] =value; // Insert the new element at the rear
+printf("Inserted %d into the queue\n", value);
+}
